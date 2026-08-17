aa05bd37d83dc6dfde73ff5b0eb8016e15972c5f
nikollas-dh <nikollasdheyvis17@gmail.com>
2026-08-17 16:11:26 -0300

    listando cardápio por restaurante

---
+++ API-Cardapio/Controllers/CardapioController.cs
@@ -0,0 +1,45 @@
+﻿using API_Cardapio.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Cardapio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardapioController : ControllerBase
+    {
+        private readonly DbCardapioContext ct = new DbCardapioContext();
+
+        public CardapioController(DbCardapioContext ct) 
+        { 
+            this.ct = ct;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult CardapioRestauranteId([FromRoute]int id)
+        {
+            try 
+            { 
+               var cardapioRestaurante = ct.Cardapios.Where(r => r.RestauranteId == id).Select(c => new
+               {
+                   c.Id,
+                   Restaurante = c.Restaurante.Nome,
+                   c.Prato.Nome,
+                   c.Prato.Descricao,
+                   c.Valor,
+                   c.Prato.Foto,
+                   Curtidas = ct.ClienteCurtidas.Where(like=>like.IdPrato==c.PratoId).Count(),
+               }).ToList();
+                if (cardapioRestaurante.Count ==0) 
+                { 
+                    return NotFound("Restaurante não possue pratos cadastrados no sistema.");
+                }
+               return Ok (cardapioRestaurante);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Erro interno no servidor!");
+            }
+        }
+    }
+}
--- API-Cardapio/obj/Debug/net10.0/API-Cardapio.AssemblyInfo.cs
+++ API-Cardapio/obj/Debug/net10.0/API-Cardapio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Cardapio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc82e5f955d92f774db7034b93392b5f25cdd929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699564fc61e5adb02da70e00a3e543064d97343d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Cardapio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Cardapio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
