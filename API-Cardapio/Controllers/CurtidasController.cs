77a0cf49747b20918702aec7b6b9fe87babe7bf0
nikollas-dh <nikollasdheyvis17@gmail.com>
2026-08-18 11:14:44 -0300

    api curtidas funcionando

--- API-Cardapio/Controllers/CurtidasController.cs
+++ API-Cardapio/Controllers/CurtidasController.cs
@@ -15,28 +15,52 @@ namespace API_Cardapio.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetCurtidas() 
+        public IActionResult Ranking() 
         {
             try
             {
                 var pratosMaisCurtidos = ct.Pratos
                     .Select(p => new
                     {
-                        p.Id,
-                        p.Nome,
-                        p.Foto,
-                        Curtidas = p.ClienteCurtida.Count()
+                       Id = p.Id,
+                       Nome = p.Nome,
+                       Foto =  p.Foto,
+                       Curtidas = p.ClienteCurtida.Count()
                     })
                     .OrderByDescending(p => p.Curtidas)
                     .Take(3)
                     .ToList();
-
+                if (!pratosMaisCurtidos.Any()) return NotFound();
                 return Ok(pratosMaisCurtidos);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500,"Erro interno no servidor");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CurtirDescurtir([FromBody] ClienteCurtida curtida)
+        {
+            try
+            {
+                var curtidaBanco = ct.ClienteCurtidas
+                    .FirstOrDefault(c => c.IdCliente == curtida.IdCliente && c.IdPrato == curtida.IdPrato);
+                
+                if (curtidaBanco != null)
+                {
+                    ct.ClienteCurtidas.Remove(curtidaBanco);
+                    ct.SaveChanges();
+                    return StatusCode(204, "Curtida removida");
+                }
+                ct.ClienteCurtidas.Add(curtida);
+                ct.SaveChanges();
+               return Ok(curtida);
             }
             catch (Exception)
             {
 
-                throw;
+                return StatusCode(500,"Erro interno no servidor");
             }
         }
 
--- API-Cardapio/obj/Debug/net10.0/API-Cardapio.AssemblyInfo.cs
+++ API-Cardapio/obj/Debug/net10.0/API-Cardapio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Cardapio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa05bd37d83dc6dfde73ff5b0eb8016e15972c5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1db422c73bc79e7b78fd91bb2513f651f433e81")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Cardapio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Cardapio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
